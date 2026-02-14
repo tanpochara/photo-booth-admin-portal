9640b9dc229f8c0d68b3c043b94ec591f081b4c3
tanpochara <pochara.youcharoen@gmail.com>
2026-02-14 20:16:12 +0700

    update openapi spec

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,11 +9,11 @@ export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { ChildFrameResponseDto } from './models/ChildFrameResponseDto';
 export type { CreateFrameRequestDto } from './models/CreateFrameRequestDto';
-export type { DetailedFrameResponseDto } from './models/DetailedFrameResponseDto';
+export { DetailedFrameResponseDto } from './models/DetailedFrameResponseDto';
 export { EditAssetsRequestDto } from './models/EditAssetsRequestDto';
 export { EditImageCoordinateRequestDto } from './models/EditImageCoordinateRequestDto';
 export type { EditImageCoordinatesRequestDto } from './models/EditImageCoordinatesRequestDto';
-export type { EditOverviewRequestDto } from './models/EditOverviewRequestDto';
+export { EditOverviewRequestDto } from './models/EditOverviewRequestDto';
 export type { FrameResponseDto } from './models/FrameResponseDto';
 export type { GenerateQrDto } from './models/GenerateQrDto';
 export type { GetResultsRequestDto } from './models/GetResultsRequestDto';
